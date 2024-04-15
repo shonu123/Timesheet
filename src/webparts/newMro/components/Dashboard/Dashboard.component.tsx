c59a59625b0c04c09b7e3b90f5b0fed3ab137dd8
JillaSridatta <jillasridatta@gmail.com>
2024-04-15 13:19:38 +0530

    css changes

--- src/webparts/newMro/components/Dashboard/Dashboard.component.tsx
+++ src/webparts/newMro/components/Dashboard/Dashboard.component.tsx
@@ -225,18 +225,7 @@ class Dashboard extends React.Component<DashboardProps, DashboardState> {
             <React.Fragment>
             
             {this.state.isEmployeeConfigured&&<div id="content" className="content p-2 pt-2">
-                <div id="clickMenu" className="menu-icon-outer" onClick={(event) => this.onMenuItemClick(event)}>
-                    <div className="menu-icon">
-                        <span>
-                        </span>
-                        <span>
-                        </span>
-                        <span>
-                        </span>
-                    </div>
-                </div>
                 {highlightCurrentNav("liDashboard")}
-
                 <div id="content" className="content p-2 pt-2">
                 <div className="container-fluid">
                     <div className='FormContent'>
